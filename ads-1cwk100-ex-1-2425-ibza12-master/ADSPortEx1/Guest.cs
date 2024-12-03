b16dd8c7f0d76328f7cae41e10bba1639cc9d458
Ibsa <ibsa9118@gmail.com>
2024-12-03 19:02:04 +0000

    Add files via upload

---
+++ ads-1cwk100-ex-1-2425-ibza12-master/ADSPortEx1/Guest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSPortEx1
+{
+
+    //Guest class implementation for use with 
+    // GuestQueue structure
+
+    //Do not delete the current function definitions, just replace the exceptions with the required functionality...
+
+    //See material from 'C# Classes' in week 2 to 
+    // aid with implementation...
+
+    class Guest : IComparable
+    {
+
+        private string name;
+        private double funds;
+
+        public Guest(string name, double funds)
+        {
+            this.name = name;
+            this.funds = funds;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Funds
+        {
+            get { return funds; }
+            set { funds = value; }
+        }
+
+        public int CompareTo(object obj)
+        {
+            Guest other = (Guest)obj;
+            return Name.compareTo(other.Name);
+        }
+
+
+    }// End of class...
+}
